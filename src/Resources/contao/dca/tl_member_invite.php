b0a0aceca40cdb1a819c429e227b7a323656e844
fritzmg <fmg@inspiredminds.at>
2021-02-22 20:13:27 +0000

    increase default max length

--- src/Resources/contao/dca/tl_member_invite.php
+++ src/Resources/contao/dca/tl_member_invite.php
@@ -92,7 +92,7 @@ $GLOBALS['TL_DCA']['tl_member_invite'] = [
         'message' => [
             'exclude' => true,
             'inputType' => 'textarea',
-            'eval' => ['tl_class' => 'clr', 'maxlength' => 2048, 'feEditable' => true],
+            'eval' => ['tl_class' => 'clr', 'maxlength' => 4096, 'feEditable' => true],
             'sql' => ['type' => 'text', 'notnull' => false],
         ],
         'date_invited' => [
